6f2f746ef490836f750182449a9ee91a883c5213
Lucas <lukaskaua172003@gmail.com>
2025-11-02 09:57:49 -0300

    feat(Adaptador): implementar o método onCreateViewHolder com um objeto View que recebe o recurso de layout

--- app/src/main/java/com/example/app_loja/Adaptador.java
+++ app/src/main/java/com/example/app_loja/Adaptador.java
@@ -1,5 +1,6 @@
 package com.example.app_loja;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,16 +8,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class Adaptador extends RecyclerView.Adapter<Adaptador.NossoViewHolder>{
+
+    private List<Produto> mListProduto;
 
     @NonNull
     @Override
-    public Adaptador.NossoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public NossoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Declaramos um objeto View com o layout que será exibido para cada item
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.itens, parent, false);
+        // Declaramos um objeto NossoViewHolder e passamos como parâmetro o objeto View recém-criado
+        NossoViewHolder viewHolder = new NossoViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Adaptador.NossoViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NossoViewHolder holder, int position) {
         // Este método recebe dois parâmetros
         // O primeiro é um objeto NossoViewHolder que contém todas as referências do layout
         // O segundo é a posição de cada item da listagem
