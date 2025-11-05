7c3141d952c119dada9cccce1fe6b9b72a51916a
Lucas <lukaskaua172003@gmail.com>
2025-11-05 17:34:14 -0300

    feat: adiciona RecyclerView à MainActivity para exibir lista de produtos com layout linear e divisores entre itens. Inicializa adapter com dados mockados.

--- app/src/main/java/com/example/app_loja/MainActivity.java
+++ app/src/main/java/com/example/app_loja/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<Produto> mListProduto;
+    RecyclerView rvListagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
         mListProduto.add(new Produto("Tablet", 3600f));
         mListProduto.add(new Produto("Tv 50", 2100f));
 
+        //instancia o RecyclerView
+        rvListagem = findViewById(R.id.rvListagem);
+        //define um tamanho fixo no layout
+        rvListagem.setHasFixedSize(true);
+
+        // Define como os itens serão exibidos no RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvListagem.setLayoutManager(layoutManager);
+
+        // Adiciona uma linha entre cada item da listagem
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+
+        //define adapter para recycler view
+        Adaptador adaptador = new Adaptador();
+        rvListagem.setAdapter(adaptador);
+
+        //atualiza todos os itens para o adaptador
+        adaptador.atualizarListagemCompleta(mListProduto);
     }
 }
